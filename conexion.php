c2baa8de707d99b1cf49c23b31661b69ee50089a
ignaciozhetta <zagu4988@gmail.com>
2023-01-07 17:19:30 -0300

    conection db

--- conexion.php
+++ conexion.php
@@ -1,19 +1,12 @@
 <?php
 
-    // require __DIR__ . '/vendor/autoload.php';
+    require __DIR__ . '/vendor/autoload.php';
 
-    // $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-    // $dotenv->load();
-
-    // Variables de entorno para producción
-    $HOST = $_ENV['HOST'];
-    $DB_USER = $_ENV['DB_USER'];
-    $DB_PASSWORD = $_ENV['DB_PASSWORD'];
-    $DB_NAME = $_ENV['DB_NAME'];
-    $DB_PORT = $_ENV['DB_PORT'];
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
 
     try {
-        $conexionDB = new mysqli("$HOST", "$DB_USER", "$DB_PASSWORD", "$DB_NAME", "$DB_PORT");
+        $conexionDB = new mysqli($_ENV['HOST'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD'], $_ENV['DB_NAME'], $_ENV['DB_PORT']);
         if ($conexionDB->connect_error){
             die("Ocurrió un error al conectar la base de datos!");
         }
