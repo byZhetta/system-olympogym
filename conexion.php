7b72f134650c3b3813e1bda1028e74505135bc2b
ignaciozhetta <zagu4988@gmail.com>
2023-01-12 12:21:23 -0300

    conection db

--- conexion.php
+++ conexion.php
@@ -5,8 +5,15 @@
     // $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
     // $dotenv->load();
 
+    // Variables de producción
+    $HOST = $_ENV['HOST'];
+    $DB_USER = $_ENV['DB_USER'];
+    $DB_PASSWORD = $_ENV['DB_PASSWORD'];
+    $DB_NAME = $_ENV['DB_NAME'];
+    $DB_PORT = $_ENV['DB_PORT'];
+
     try {
-        $conexionDB = new mysqli($_ENV['HOST'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD'], $_ENV['DB_NAME'], $_ENV['DB_PORT']);
+        $conexionDB = new mysqli("$HOST", "$DB_USER", "$DB_PASSWORD", "$DB_NAME", "$DB_PORT");
         if ($conexionDB->connect_error){
             die("Ocurrió un error al conectar la base de datos!");
         }
