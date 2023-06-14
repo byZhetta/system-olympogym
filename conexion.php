c6d8e5b8f6c95d3ab226c8add45c7ced9e691b29
Roani Campos <jcamposg15@unc.edu.pe>
2023-06-13 21:02:56 -0500

    Se modificó la conexion a BD

--- conexion.php
+++ conexion.php
@@ -6,14 +6,13 @@
     // $dotenv->load();
 
     // Variables de producción
-    $HOST = $_ENV['HOST'];
-    $DB_USER = $_ENV['DB_USER'];
-    $DB_PASSWORD = $_ENV['DB_PASSWORD'];
-    $DB_NAME = $_ENV['DB_NAME'];
-    $DB_PORT = $_ENV['DB_PORT'];
+    $HOST = "localhost";
+    $DB_USER = "root";
+    $DB_PASSWORD = "";
+    $DB_NAME = "db_gym";
 
     try {
-        $conexionDB = new mysqli("$HOST", "$DB_USER", "$DB_PASSWORD", "$DB_NAME", "$DB_PORT");
+        $conexionDB = new mysqli("$HOST", "$DB_USER", "$DB_PASSWORD", "$DB_NAME");
         if ($conexionDB->connect_error){
             die("Ocurrió un error al conectar la base de datos!");
         }
